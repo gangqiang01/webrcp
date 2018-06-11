8496763c94179b8033020681c55ee43bbd2e26e7
gangqiang.sun <gangqiang.sun@advantech.com.cn>
2018-06-11 10:41:13 +0800

    add icon png

--- frontend/web/assets/js/index.js
+++ frontend/web/assets/js/index.js
@@ -3,11 +3,7 @@ var map;
 $(function() {
 	LoginStatus("UserDuedateCheck","index.html");
 	SetHTML("barset_index");
-	if(localStorage.getItem("Company") === "Guest"){
-		document.getElementById("account").innerHTML = '<a class="alert-a" href="profile.html" style="color:#3c763d;">View Details <i class="fa fa-arrow-circle-right"></i></a>';
-	}else{
-		document.getElementById("account").innerHTML = '<a class="alert-a" href="management.html" style="color:#3c763d;">View Details <i class="fa fa-arrow-circle-right"></i></a>';
-	}
+
 	$('.alert').css( 'cursor', 'pointer' );
 	$(".alert").hover(function () {
 		$(this,".alert").addClass("bw");
